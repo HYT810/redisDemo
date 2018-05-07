007fef8828626bca0850177e10ef6b9abf9e90b5
hyt <497473811@qq.com>
2018-05-08 00:01:37 +0800

    添加项目文件。

---
+++ RedisDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedisDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //测试
+            Console.WriteLine("使用redis存放数据");
+            //RedisHelper.Add<string>("name", "hyt", DateTime.Now.AddDays(1));
+            //RedisHelper.Add<string>("Age", "28", DateTime.Now.AddDays(1));
+            Console.WriteLine(RedisHelper.Get<string>("name").ToString());
+            Console.ReadKey();
+        }
+    }
+}
